f7eab0afa057f1200776dea69b92166e312d440b
Jayasri Patnala <jayasripatnala11@gmail.com>
2026-07-16 12:25:29 +0530

    Add files via upload

---
+++ Spring Core and Maven/Mandatory hands-on/Creating and Configuring a Maven Project/README.md
@@ -0,0 +1,3 @@
+# Exercise 4
+
+Import this project as an Existing Maven Project. Run `mvn compile` to verify dependencies.
---
+++ Spring Core and Maven/Mandatory hands-on/Creating and Configuring a Maven Project/src/main/java/com/library/service/LibraryInfo.java
@@ -0,0 +1,8 @@
+package com.library.service;
+
+public class LibraryInfo {
+    public static void main(String[] args){
+        System.out.println("LibraryManagement Maven project configured successfully.");
+        System.out.println("Spring Context, Spring AOP and Spring WebMVC dependencies are available.");
+    }
+}
