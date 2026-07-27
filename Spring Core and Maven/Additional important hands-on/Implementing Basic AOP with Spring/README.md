239fe16bc89111c8da976925f35d2e208a711d45
Jayasri Patnala <jayasripatnala11@gmail.com>
2026-07-27 16:46:47 +0530

    Add files via upload

---
+++ Spring Core and Maven/Additional important hands-on/Implementing Basic AOP with Spring/README.md
@@ -0,0 +1,6 @@
+# Exercise 8: Implementing Basic AOP with Spring
+
+Expected Output:
+Before method execution
+Book issued successfully.
+After method execution
---
+++ Spring Core and Maven/Additional important hands-on/Implementing Basic AOP with Spring/src/main/java/com/library/service/BookService.java
@@ -0,0 +1,1 @@
+package com.library.service; import org.springframework.stereotype.Service; @Service public class BookService{ public void issueBook(){ System.out.println("Book issued successfully."); }}
